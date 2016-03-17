5e03b74db8444b9ef3308cba9586a64d87414c9e
Jake Lim <jervie.lim@gmail.com>
2016-03-17 16:20:16 +0800

    Adds associations between tables

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,14 @@
 class Article < ActiveRecord::Base
-  validates :title, presence: true, length: {minimum: 3, maximum: 50}
-  validates :description, presence: true, length: {minimum: 3, maximum: 500}
+  belongs_to :user
+
+  validates :title, 
+            presence: true, 
+            length: {minimum: 3, maximum: 50}
+  validates :description, 
+            presence: true, 
+            length: {minimum: 3, maximum: 500}
+
+  validates :user_id,
+            presence: true #requires that everytime an article is created, a user_id is associated
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,26 @@
 class User < ActiveRecord::Base
+  has_many :articles
+  before_save {self.email = email.downcase} #turns all email values entered into lowercase
+
   validates :username, 
             presence: true, 
             length: {minimum: 3, maximum: 25}, 
             uniqueness: {case_sensitive: false}
 
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+#   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates_format_of :email, :with => /@/
 
   validates :email, 
             presence: true, 
             uniqueness: {case_sensitive: false},
-            length: { maximum: 200}, 
-            format: { with: VALID_EMAIL_REGEX }
+            length: { maximum: 200}
+ #          format: { with: VALID_EMAIL_REGEX }
+
 end
+
+
+#validate :cant_be_something
+#def cant_be_something
+#  errors.add(:string, "can't be something") if self.string == "something"
+# end
