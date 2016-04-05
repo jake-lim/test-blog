f93b1d41fc720c59d93fccae53e5aeac0d799bda
Jake Lim <jervie.lim@gmail.com>
2016-04-05 17:23:57 +0800

    Creates show action and add gravatar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+before_action :set_user, only:[:show, :edit, :update]
+
   def new 
     @user = User.new
   end
@@ -14,11 +16,10 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
+
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update
         flash[:success] = "Your account was successfully updated"
         redirect_to articles_path
@@ -27,7 +28,15 @@ class UsersController < ApplicationController
     end
   end
 
+  def show
+    
+  end
+
   private
+  def set_user
+    @user = User.find(params[:id])
+  end
+
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def gravatar_for(user, options = { size: 80})
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    size = options[:size]
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    image_tag(gravatar_url, alt: user.username, class: "img-circle")
+  end
 end
+
+#gravatar iurl based on md5 hash of user's email address
+#implemented using hex digest method
